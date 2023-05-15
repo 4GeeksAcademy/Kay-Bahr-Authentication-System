ea65758441bc974af9d1882526666103ee010fe0
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-15 02:50:52 +0000

    nav update commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,12 +6,9 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Authentication System</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
 					<Link to="/login">
 						<button className="btn btn-primary">Login</button>
 					</Link>
