d4682be09d5b066cc0c4350a2a093438c0876fe1
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-22 03:47:52 +0000

    JWT install commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,21 +1,26 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/token', methods=['POST'])
-def handle_token():
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
 
-    response_body = {
-        "message": "This is the backend to generates a token only if it receives an email and password that matches in the database, and will return the token to the front-end if everything is ok."
-    }
-
-    return jsonify(response_body), 200
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
 # Then, on every other endpoint in your database you will have to validate if the token exists in the request header and if it does you will have to validate it
 
