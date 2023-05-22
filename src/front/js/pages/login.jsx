b631c805a969d66a83beba4ab94f170bf616a06e
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-05-22 04:00:38 +0000

    login style commit

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -6,14 +6,16 @@ export function Login() {
     return (
         <div id="endPointBody">
             <form>
+                <h1 class="text-center" id='loginH1'>
+                    Login
+                </h1>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1" />
